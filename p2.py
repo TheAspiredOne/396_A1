32c1d68b635caf9a9f22d35810f268594739bf23
Tan Avery <altan@ualberta.ca>
2017-09-20 18:03:09 -0600

    added basic tree implementation

---
+++ p2.py
@@ -0,0 +1,17 @@
+class Node(object):
+    def __init__(self, value):
+        self.value = value
+        self.children = []
+
+
+
+    def add_child(self, new_node):
+        self.children.append(new_node)
+
+
+
+input_list = input()
+
+
+
+
